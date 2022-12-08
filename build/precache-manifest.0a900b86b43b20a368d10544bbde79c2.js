95731639d898e201f614d0eb61fe52d957cd2ee5
Dejan <dejanvukovic1971@gmail.com>
2022-12-08 09:55:55 +0100

    abc

---
+++ build/precache-manifest.0a900b86b43b20a368d10544bbde79c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69578b49a2c3abaed0fd675648c320af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "956f861b824e76ec9382",
+    "url": "/static/css/main.d0b2bf15.chunk.css"
+  },
+  {
+    "revision": "ce5ecb2962c3f21329ee",
+    "url": "/static/js/2.57707a8c.chunk.js"
+  },
+  {
+    "revision": "956f861b824e76ec9382",
+    "url": "/static/js/main.74db1ae8.chunk.js"
+  },
+  {
+    "revision": "ce4ac8adeef2eaf462ca",
+    "url": "/static/js/runtime-main.d85d5214.js"
+  }
+]);
